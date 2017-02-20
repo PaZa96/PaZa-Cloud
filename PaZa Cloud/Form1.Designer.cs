b3f83a294f56832d948f28aea9817b9336a67801
Pavel Zhavoronok <pahazavoronok@gmail.com>
2017-02-20 17:33:04 +0300

    first commit

---
+++ PaZa Cloud/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PaZa_Cloud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("root");
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.создатьПапкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(463, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 33);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "root";
+            treeNode1.Text = "root";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(445, 276);
+            this.treeView1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьПапкуToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(687, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // создатьПапкуToolStripMenuItem
+            // 
+            this.создатьПапкуToolStripMenuItem.Name = "создатьПапкуToolStripMenuItem";
+            this.создатьПапкуToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.создатьПапкуToolStripMenuItem.Text = "Создать папку";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(463, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(463, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(463, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(463, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(463, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 321);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem создатьПапкуToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ PaZa Cloud/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dropbox.Api;
+using Dropbox.Api.Users;
+using Dropbox.Api.Files;
+using System.Linq;
+
+namespace PaZa_Cloud
+{
+    public partial class Form1 : Form
+    {
+        static string token = "8FP_ykdhlGAAAAAAAAAATkk6ay4PQvMhxnE5RiEI2zmnVnGI0a8ZTBa18N0LOxLW";
+        static DropboxClient client = new DropboxClient(token);
+
+        public  Form1()
+        {
+            getFullInfo();
+            getSpaceUsage();
+            getNode();
+            InitializeComponent();
+        }
+
+        private async void getFullInfo()
+        {
+            FullAccount info = await client.Users.GetCurrentAccountAsync();
+            if (info != null)
+            {
+                label1.Text = info.Name.DisplayName;
+                label2.Text = "Email: " + info.Email;
+                label3.Text = "Страна: " + info.Country;
+                label4.Text = "Подписка: " + (info.AccountType.IsBasic ? "Basic" : info.AccountType.IsBusiness ? "Business" : info.AccountType.IsPro ? "Pro" : "Unknown");
+            }
+        }
+
+        private async void getSpaceUsage()
+        {
+            SpaceUsage space = await client.Users.GetSpaceUsageAsync();
+            if (space != null)
+            {
+                label5.Text = "Используется: " + (space.Used / 1000000).ToString() + "MB";
+                label6.Text = "Всего: " + (space.Allocation.AsIndividual.Value.Allocated / 1000000).ToString() + "MB";
+            }
+        }
+
+        public string[] getNodeName(string path, int pos)
+        {
+            string[] res = new string[2];
+            int spos = path.LastIndexOf("/");
+            res[1] = path.Substring(spos + 1, path.Length - spos - 1);
+            path = path.Remove(spos, path.Length - spos);
+            spos = path.LastIndexOf("/") + 1;
+            res[0] = path.Substring(spos, path.Length - spos);
+            return res;
+        }
+
+        public async void getNode()
+        {
+            ListFolderResult list = await client.Files.ListFolderAsync(new ListFolderArg(string.Empty, true));
+            if (list != null)
+            {
+                TreeNode[] root = treeView1.Nodes.Find("root", false);
+                root[0].Nodes.Clear();
+                foreach (var item in list.Entries.Where(i => i.IsFolder))
+                {
+                    int pos = item.PathDisplay.LastIndexOf("/");
+                    if (pos == 0)
+                    {
+                        treeView1.BeginUpdate();
+                        root[0].Nodes.Add(item.Name, item.Name);
+                        treeView1.EndUpdate();
+                    }
+                    else
+                    {
+                        string[] names = getNodeName(item.PathDisplay, pos);
+                        TreeNode[] node = root[0].Nodes.Find(names[0], true);
+                        treeView1.BeginUpdate();
+                        node[0].Nodes.Add(names[1], names[1]);
+                        treeView1.EndUpdate();
+                    }
+                }
+
+                foreach (var item in list.Entries.Where(i => i.IsFile))
+                {
+                    int pos = item.PathDisplay.LastIndexOf("/");
+                    if (pos == 0)
+                    {
+                        treeView1.BeginUpdate();
+                        root[0].Nodes.Add(item.Name, item.Name);
+                        treeView1.EndUpdate();
+                    }
+                    else
+                    {
+                        string[] names = getNodeName(item.PathDisplay, pos);
+                        TreeNode[] node = root[0].Nodes.Find(names[0], true);
+                        treeView1.BeginUpdate();
+                        node[0].Nodes.Add(names[1], names[1]);
+                        treeView1.EndUpdate();
+                    }
+                    treeView1.ExpandAll();
+                }
+            }
+        }
+
+        private void Download()
+        {
+
+        }
+
+    }
+}
